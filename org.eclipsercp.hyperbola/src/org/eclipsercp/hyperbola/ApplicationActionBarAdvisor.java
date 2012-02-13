78394e75b461370a97b7b34553950cfeb47f68b4
paven <patrik.gustafsson@purplescout.se>
2012-02-13 15:00:36 +0100

    Chapter 6 p 88

--- org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/ApplicationActionBarAdvisor.java
+++ org.eclipsercp.hyperbola/src/org/eclipsercp/hyperbola/ApplicationActionBarAdvisor.java
@@ -16,21 +16,41 @@
  *******************************************************************************/
 package org.eclipsercp.hyperbola;
 
+import org.eclipse.jface.action.GroupMarker;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
+import org.eclipse.ui.internal.about.AboutAction;
 
 public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
+
+	private IWorkbenchAction exitAction;
+	private IWorkbenchAction aboutAction;
 
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 		super(configurer);
 	}
 
 	protected void makeActions(IWorkbenchWindow window) {
+		exitAction = ActionFactory.QUIT.create(window);
+		register(exitAction);
+		aboutAction = ActionFactory.ABOUT.create(window);
+		register(aboutAction);
 	}
 
 	protected void fillMenuBar(IMenuManager menuBar) {
+		MenuManager hyperbolaMenu = new MenuManager("&Purple", "purple");
+		hyperbolaMenu.add(exitAction);
+		MenuManager helpMenu = new MenuManager("&Help", "help");
+		helpMenu.add(aboutAction);
+		menuBar.add(hyperbolaMenu);
+		hyperbolaMenu.add(helpMenu);
+
+		
 	}
 
 }
